f0a106f1c5b0e2e1e01522578c197b2c0457cc5b
hareeshdhruva <hareeshdhruva123@gmail.com>
2024-02-19 03:51:20 +0530

    osm.v.1.1

--- message/index.js
+++ message/index.js
@@ -33,7 +33,7 @@ const pub = new Radis(process.env.REDIS);
 const sub = new Radis(process.env.REDIS);
 
 message.get('/',(req,res)=>{
-  res.render("osm redis server")
+  res.send("osm redis server")
 })
 
 io.on("connection",(socket)=>{
